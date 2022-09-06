51558d886a68b9cc7096e7444cb3829018f0dab7
Brandon Lackey <44684885+nwohater@users.noreply.github.com>
2022-09-06 10:38:04 -0700

    first

---
+++ deck.go
@@ -0,0 +1,28 @@
+package main
+
+// Create a new type of 'deck'
+// which is a slice of strings
+type deck []string
+
+// new deck function
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
+// receiver print function
+func (d deck) print() {
+	for i, card := range d {
+		println(i, card)
+	}
+}
